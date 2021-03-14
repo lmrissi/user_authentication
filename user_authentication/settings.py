f4e927ba1adfc72ebba1b0df5b65988438ef9117
lmrissi <leonardomelorissi@gmail.com>
2021-03-14 17:52:44 -0300

    Envio do projeto 2

--- user_authentication/settings.py
+++ user_authentication/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '74$0*8bsi1)=&#i_hj1wjfn+y-gx8spl%2f0j*bwk!jardjy2q'
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
